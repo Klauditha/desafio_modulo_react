8df5a2d5320fce5ee01e00dbc43b07f72291ed77
Claudia Villarroel <claudia.villarroel@live.com>
2023-10-10 23:06:11 -0300

    Ejemplo proyecto tailwind css

---
+++ 04_ConsumoAPIs/Tutoria/my-project/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Layout from './components/Layout'
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+    Hello world!
+    <Layout/>
+  </h1>
+  )
+}
+
+export default App
